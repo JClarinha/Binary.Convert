597239df2c91ba3a22105b5d98124c1376718b50
JClarinha <joaoclarinha13@gmail.com>
2025-02-27 15:41:14 +0000

    Adicionada conversão de binário para decimal

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿using System.ComponentModel.Design;
-
+using System.Threading.Channels;
 
 Menu();
+
+
 
 
 
@@ -32,18 +34,24 @@ static void Menu()
 {
     Console.WriteLine("Bem vindo ao Converter!");
     Console.WriteLine("O que gostaria de fazer?");
-    Console.WriteLine("1 - Decimal to Binary;");
-    Console.WriteLine("2 - Sair.");
+    Console.WriteLine("1 - Decimal para Binario;");
+    Console.WriteLine("2 - Binario para Decimal;");
+    Console.WriteLine("3 - Sair.");
 
     int x = int.Parse(Console.ReadLine());
 
     switch (x)
     {
         case 1:
-            Convert();
+            ConvertDToB();
             break;
 
         case 2:
+            ConvertBToD();
+                break;
+
+
+        case 3:
             break;
 
         default:
@@ -57,7 +65,7 @@ static void Menu()
 
 
 
-static void Convert()
+static void ConvertDToB()
 {
     Console.Write("Introduza um número decimal para converter para binário: ");
     if (!int.TryParse(Console.ReadLine(), out int x))
@@ -65,7 +73,7 @@ static void Convert()
         Console.WriteLine("Número inválido!");
         Console.WriteLine();
         Console.WriteLine("Por favor introduz um decimal válido!");
-        Convert();
+        ConvertDToB();
         
 
     }
@@ -98,4 +106,79 @@ static void Convert()
     }
     Console.WriteLine();
     Menu();
+}
+
+
+static void ConvertBToD()
+{
+    Console.WriteLine("Introduz o Binario (0 ou 1) que pretende converter para Decimal, quando terminar carregue no enter!");
+
+    List<int> bn = new List<int>();
+
+
+    BNAdd(bn);
+    bn.Reverse();
+    BNT(bn);
+
+
+
+
+    
+
+    if (bn.Count > 0)
+    {
+        for (int i = bn.Count - 1; i >= 0; i--)
+        {
+            Console.WriteLine("indice " + i + ": " + bn[i]);
+        }
+    }
+
+
+
+    static void BNAdd(List<int> bn)
+    {
+        string input = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(input))
+        {
+            return;
+        }
+
+        foreach (char c in input)
+        {
+            if (c == '0' || c == '1')
+            {
+                bn.Add(c - '0');
+            }
+            else
+            {
+                Console.WriteLine("Carácter inválido! Apenas 0 ou 1 são permitidos. Tente novamente.");
+                BNAdd(bn);
+                return;
+            }
+        }
+
+
+       
+
+
+    }
+
+    
+
+    static void BNT(List<int> bn)
+    {
+        int dc = 0;
+
+        for (int j = bn.Count - 1; j >= 0; j--)
+        {
+            int rsl = (int)Math.Pow(2, j) * bn[j];
+            dc = dc + rsl;
+        }
+
+        Console.WriteLine(dc);
+    }
+
+    Menu();
+
 }
